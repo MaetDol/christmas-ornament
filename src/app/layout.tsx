b247907262c659575860bdff6300cc563a0b7f94
MaetDol <rndka9@gmail.com>
2024-12-15 21:18:17 +0900

    feat, 뷰포트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=no"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${ownglyph.variable} antialiased`}
       >
