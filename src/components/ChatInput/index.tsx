3a4888954cc1a0677ec1668d15f54af81cccde05
MaetDol <rndka9@gmail.com>
2024-12-15 22:44:07 +0900

    feat, textarea 로 변경
    contenteditable 로 하니, placeholder 등의 컨트롤이 쉽지 않아 수정했어요.
    
    이후 고도화가 필요하다면 변경하도록 하겠습니다.

--- src/components/ChatInput/index.tsx
+++ src/components/ChatInput/index.tsx
@@ -1,22 +1,42 @@
 import AirplanSVG from '@/static/svg/solid-paper-airplane.svg';
 import Image from 'next/image';
+import { useState } from 'react';
 
-export function ChatInput() {
+interface Props {
+  onSubmit: (message: string) => void;
+}
+
+export function ChatInput({ onSubmit }: Props) {
+  const [input, setInput] = useState('');
+
+  const submit = () => {
+    onSubmit(input);
+    setInput('');
+  };
+
   return (
     <div className="w-full mt-auto p-2 bg-rose-800 flex gap-2">
-      <div
+      <textarea
         contentEditable
         className="py-2 px-4 rounded-full bg-slate-50 flex-1 h-10
           shadow-[inset_2px_-2px_4px_rgba(0,0,0,0.1)] 
           font-ownglyph h-fit text-slate-800
           "
-      ></div>
+        style={{ resize: 'none' }}
+        rows={1}
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+        placeholder="메시지를 입력하세요"
+      ></textarea>
+
       <button
         className="bg-slate-50 shrink-0 w-10 h-10 rounded-full
           shadow-[inset_2px_-2px_4px_rgba(0,0,0,0.1)]
           flex items-center justify-center font-[18px]
         "
+        onClick={submit}
       >
+        <span className="sr-only">메시지 보내기</span>
         <Image src={AirplanSVG} width={32} height={32} alt="submit" />
       </button>
     </div>
