7bd8d4e48f972d3b09e1ddb4bd1705eb0604bce6
맷돌 <maetdol@maesdol-ui-Macmini.local>
2024-12-25 00:41:43 +0900

    feat, 이미지 상수화

---
+++ src/shared/constants/mbtis/images.ts
@@ -0,0 +1,35 @@
+import infp from "@/static/images/infp_giftbox.png";
+import enfp from "@/static/images/enfp_star.png";
+import intj from "@/static/images/intj_sock.png";
+import entj from "@/static/images/entj_rudolph.png";
+import infj from "@/static/images/infj_santa.png";
+import enfj from "@/static/images/enfj_wreath.png";
+import intp from "@/static/images/intp_snowflake.png";
+import entp from "@/static/images/entp_bell.png";
+import isfp from "@/static/images/isfp_candy-cane.png";
+import esfp from "@/static/images/esfp_fairy-lights.png";
+import istp from "@/static/images/istp_snowman.png";
+import estp from "@/static/images/estp_tree.png";
+import isfj from "@/static/images/isfj_gingerbread.png";
+import esfj from "@/static/images/esfj_angel.png";
+import istj from "@/static/images/istj_ball-ornaments.png";
+import estj from "@/static/images/estj_ribbon.png";
+
+export const MBTIImages = {
+  INFP: infp,
+  ENFP: enfp,
+  INTJ: intj,
+  ENTJ: entj,
+  INFJ: infj,
+  ENFJ: enfj,
+  INTP: intp,
+  ENTP: entp,
+  ISFP: isfp,
+  ESFP: esfp,
+  ISTP: istp,
+  ESTP: estp,
+  ISFJ: isfj,
+  ESFJ: esfj,
+  ISTJ: istj,
+  ESTJ: estj,
+} as const;
