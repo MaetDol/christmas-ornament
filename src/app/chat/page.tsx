7c9ab88daa408a2a3f88a2846f9cd73af4fe082b
맷돌 <maetdol@maesdol-ui-Macmini.local>
2024-12-24 23:17:27 +0900

    feat, Link href 로 이동하게 수정

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,6 +4,7 @@ import { Chat } from "@/components/Chat";
 import { useConversation } from "@/components/Chat/hooks/useConversation";
 import { ChatInput } from "@/components/ChatInput";
 import BackgroundImage from "@/static/images/bg.jpg";
+import { mbtiScoreService } from "@/static/services/mbtiScoreService";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -18,7 +19,8 @@ export default function Page() {
     chatRef.current?.scrollToBottom();
   }, [conversation]);
 
-  const router = useRouter();
+  console.log(mbtiScoreService.calculate(res));
+
   return (
     <main className=" w-full h-full">
       <Image
@@ -38,24 +40,27 @@ export default function Page() {
           }`}
         >
           {!isEnd && <ChatInput onSubmit={submit} />}
+        </div>
 
-          <div
-            className={`
+        <div
+          className={`
             absolute left-1/2 translate-x-[-50%] ease-out
-            transition-[top] ${isEnd ? "top-[-20vh]" : "top-[100vh]"}
+            transition-[top] ${isEnd ? "top-[82vh]" : "top-[100vh]"}
             `}
-          >
-            {isEnd && (
-              <Link
-                className="font-ownglyph text-[24px] text-center py-3 px-5 text-white
+        >
+          {isEnd && (
+            <Link
+              className="font-ownglyph text-[24px] text-center py-3 px-5 text-white
             bg-rose-800 rounded-full shadow-drop-1
             "
-                href={""}
-              >
-                카드 보러가기 !
-              </Link>
-            )}
-          </div>
+              href={{
+                pathname: "/result",
+                query: { mbti: mbtiScoreService.calculate(res) },
+              }}
+            >
+              카드 보러가기 !
+            </Link>
+          )}
         </div>
       </section>
     </main>
