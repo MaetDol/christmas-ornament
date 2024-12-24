8921046518f3fd6cefbe32e197b8c06d4389802b
맷돌 <maetdol@maesdol-ui-Macmini.local>
2024-12-24 23:31:22 +0900

    feat, 프롬프트 개선

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,6 @@
-import { OpenAIService } from '@/shared/services/openAiService';
-import { getQuestion } from '@/shared/utils/getQuestion';
-import { NextResponse } from 'next/server';
+import { OpenAIService } from "@/shared/services/openAiService";
+import { getQuestion } from "@/shared/utils/getQuestion";
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const { id, userResponse } = await request.json();
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
   if (!question) {
     return NextResponse.error();
   }
-  if (typeof userResponse !== 'string') {
+  if (typeof userResponse !== "string") {
     return NextResponse.error();
   }
   if (userResponse.length > 200) {
@@ -19,9 +19,11 @@ export async function POST(request: Request) {
   try {
     const chat = await new OpenAIService()
       .system(
-        '당신은 전문적인 심리학자입니다.' +
-          question.me +
-          '라는 질문에, 아래와 같이 답했습니다. MBTI 를 분석해주세요.'
+        "당신은 전문적인 심리학자입니다.\n" +
+          +`'${question.me}'` +
+          "\n라는 질문에, 다음과 같이 대답했을때. MBTI 를 분석하세요.\n" +
+          "성의없거나 애매한 대답일 경우, 50에 가까운 percent 와 낮은 confidence 를 부여하세요." +
+          "깐깐하고 논리적인 이유로 값을 부여해야합니다. 신중하세요."
       )
       .user(userResponse)
       .chat();
